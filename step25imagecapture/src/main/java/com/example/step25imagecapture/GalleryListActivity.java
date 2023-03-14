46441da94b27e10b3eacd44e76850f4a1d2e1062
kimgura <oli999@naver.com>
2023-03-14 11:39:50 +0900

    step25 imagecapture MyHttpUtil 완성하고 사용하는 구조로 변경중...

--- step25imagecapture/src/main/java/com/example/step25imagecapture/GalleryListActivity.java
+++ step25imagecapture/src/main/java/com/example/step25imagecapture/GalleryListActivity.java
@@ -22,7 +22,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 public class GalleryListActivity extends AppCompatActivity implements View.OnClickListener{
     ActivityGalleryListBinding binding;
